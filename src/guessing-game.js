12dfc1d657f43edde800eec131f62c057a298ae3
Maryia Vasilkova <vasmari@users.noreply.github.com>
2019-10-07 20:55:45 +0300

    feat: add methods for guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minGuess;
+        this.maxGuess;
+        this.myGuess;
+
+    }
 
     setRange(min, max) {
+        this.minGuess = min;
+        this.maxGuess = max;
 
     }
 
     guess() {
+        this.myGuess = Math.round((this.maxGuess + this.minGuess)/2);
+        return this.myGuess;
 
     }
 
     lower() {
+        this.maxGuess = this.myGuess;
 
     }
 
     greater() {
+        this.minGuess = this.myGuess;
 
     }
 }
